69ceed1a95b94464970d754f14a28c6f736548b5
Ayman M.Osama <kalabala2005@hotmail.com>
2022-05-16 13:57:10 +0200

    updates on step no 7 as Scenario

--- src/main/resources/features/F07_followUs.feature
+++ src/main/resources/features/F07_followUs.feature
@@ -3,4 +3,17 @@ Feature: user can push on followUs
 
 
   Scenario: user could open site and push follow us
-    When user hover to follow us
+    When user open facebook page
+    Then  facebook is opened or no
+
+  Scenario: user could open site and push follow us twitter
+    When user open twitter page
+    Then  twitter is opened in new tab
+
+  Scenario: user could open site and push follow rss
+    When user open rss page
+    Then  rss is opened in new tab
+
+  Scenario: user could open site and push follow youtube
+    When user open youtube page
+    Then  youtube is opened in new tab
--- src/test/java/stepDefinitions/D07_followUsStepDef.java
+++ src/test/java/stepDefinitions/D07_followUsStepDef.java
@@ -1,20 +1,19 @@
 package stepDefinitions;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+import java.util.ArrayList;
 
 public class D07_followUsStepDef {
 
 
 
-    @When("user hover to follow us")
+    @When("user open facebook page")
     public void user_hover() throws InterruptedException {
         // Locating the Main Menu (Parent element)
         WebElement followUs = Hooks.driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[1]/div[4]/div[1]/div/strong"));
@@ -22,7 +21,7 @@ public class D07_followUsStepDef {
         //Instantiating Actions class
         Actions actions = new Actions(Hooks.driver);
 
-        //Hovering on followus
+        //Hovering on Followus
         actions.moveToElement(followUs);
 
         // Locating the element from page
@@ -33,8 +32,147 @@ public class D07_followUsStepDef {
 
         //build()- used to compile all the actions into a single step
         actions.click().build().perform();
-        Thread.sleep(2000);
+        System.out.println(Hooks.driver.getCurrentUrl());
+        System.out.println(Hooks.driver.getTitle());
 
+
+        Thread.sleep(10000);
+
+    }
+
+
+    @Then("facebook is opened or no")
+
+            public void face_new_tab()
+    {
+        String url = Hooks.driver.getWindowHandle();
+        ArrayList<String> tabs = new ArrayList<> (Hooks.driver.getWindowHandles());
+        Hooks.driver.switchTo().window(tabs.get(1));
+        System.out.println(Hooks.driver.getCurrentUrl());
+        System.out.println(Hooks.driver.getTitle());
+        Assert.assertEquals(Hooks.driver.getCurrentUrl(),"https://www.facebook.com/nopCommerce");
+
+    }
+
+
+    @When("user open twitter page")
+    public void user_twitter_hover() throws InterruptedException {
+        // Locating the Main Menu (Parent element)
+        WebElement followUs = Hooks.driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[1]/div[4]/div[1]/div/strong"));
+
+        //Instantiating Actions class
+        Actions actions = new Actions(Hooks.driver);
+
+        //Hovering on Followus
+        actions.moveToElement(followUs);
+
+        // Locating the element from page
+        WebElement twitter = Hooks.driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[1]/div[4]/div[1]/ul/li[2]/a"));
+
+        //To mouseover on facebook icon
+        actions.moveToElement(twitter);
+
+        //build()- used to compile all the actions into a single step
+        actions.click().build().perform();
+        System.out.println(Hooks.driver.getCurrentUrl());
+        System.out.println(Hooks.driver.getTitle());
+
+
+        Thread.sleep(10000);
+
+    }
+
+
+    @Then("twitter is opened in new tab")
+
+    public void face_twitter_new_tab()
+    {
+        String url = Hooks.driver.getWindowHandle();
+        ArrayList<String> tabs = new ArrayList<> (Hooks.driver.getWindowHandles());
+        Hooks.driver.switchTo().window(tabs.get(1));
+        System.out.println(Hooks.driver.getCurrentUrl());
+        System.out.println(Hooks.driver.getTitle());
+        Assert.assertEquals(Hooks.driver.getCurrentUrl(),"https://twitter.com/nopCommerce");
+
+    }
+
+    @When("user open rss page")
+    public void user_rss_hover() throws InterruptedException {
+        // Locating the Main Menu (Parent element)
+        WebElement followUs = Hooks.driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[1]/div[4]/div[1]/div/strong"));
+
+        //Instantiating Actions class
+        Actions actions = new Actions(Hooks.driver);
+
+        //Hovering on Followus
+        actions.moveToElement(followUs);
+
+        // Locating the element from page
+        WebElement rss = Hooks.driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[1]/div[4]/div[1]/ul/li[3]/a"));
+
+        //To mouseover on facebook icon
+        actions.moveToElement(rss);
+
+        //build()- used to compile all the actions into a single step
+        actions.click().build().perform();
+        System.out.println(Hooks.driver.getCurrentUrl());
+
+
+        Thread.sleep(10000);
+
+    }
+
+
+    @Then("rss is opened in new tab")
+
+    public void face_rss_new_tab()
+    {
+        String url = Hooks.driver.getWindowHandle();
+        ArrayList<String> tabs = new ArrayList<> (Hooks.driver.getWindowHandles());
+        Hooks.driver.switchTo().window(tabs.get(0));
+        System.out.println(Hooks.driver.getCurrentUrl());
+        Assert.assertEquals(Hooks.driver.getCurrentUrl(),"https://demo.nopcommerce.com/news/rss/1");
+
+    }
+
+
+    @When("user open youtube page")
+    public void user_youtube_hover() throws InterruptedException {
+        // Locating the Main Menu (Parent element)
+        WebElement followUs = Hooks.driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[1]/div[4]/div[1]/div/strong"));
+
+        //Instantiating Actions class
+        Actions actions = new Actions(Hooks.driver);
+
+        //Hovering on Followus
+        actions.moveToElement(followUs);
+
+        // Locating the element from page
+        WebElement youtube = Hooks.driver.findElement(By.xpath("/html/body/div[6]/div[4]/div[1]/div[4]/div[1]/ul/li[4]/a"));
+
+        //To mouseover on facebook icon
+        actions.moveToElement(youtube);
+
+        //build()- used to compile all the actions into a single step
+        actions.click().build().perform();
+        System.out.println(Hooks.driver.getCurrentUrl());
+        System.out.println(Hooks.driver.getTitle());
+
+
+        Thread.sleep(10000);
+
+    }
+
+
+    @Then("youtube is opened in new tab")
+
+    public void face_youtube_new_tab() {
+        String url = Hooks.driver.getWindowHandle();
+        ArrayList<String> tabs = new ArrayList<>(Hooks.driver.getWindowHandles());
+        Hooks.driver.switchTo().window(tabs.get(1));
+        System.out.println(Hooks.driver.getCurrentUrl());
+        System.out.println(Hooks.driver.getTitle());
+        Assert.assertEquals(Hooks.driver.getCurrentUrl(), "https://www.youtube.com/user/nopCommerce");
     }
 
 }
